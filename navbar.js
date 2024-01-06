cd457adddffbf6129d21a4791377b2c7ad1c97f3
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-01-06 13:40:50 +0530

    Add navbar.js file

---
+++ navbar.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+function hideSidebar(){
+    let hide_sidebar = document.querySelector(".sidebar");
+    hide_sidebar.style.display = 'none';
+}
