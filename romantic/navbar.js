e13b1b96fb06d3f9ae676202a820a255c1774e1c
lord-cyclone100 <sang66deb@gmail.com>
2024-01-15 13:29:30 +0530

    Fixed genre bug

---
+++ romantic/navbar.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+function hideSidebar() {
+    let hide_sidebar = document.querySelector(".sidebar");
+    hide_sidebar.style.display = 'none';
+}
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
